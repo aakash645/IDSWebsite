801895392d788a2a9b1577d4ab3d19ca451ce05a
aakash645 <137418132+aakash645@users.noreply.github.com>
2026-02-17 16:35:03 +0530

    booking-url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,4 +7,4 @@ export { BRAND_COLORS, THEME } from './colors';
 export { MENU_ITEMS } from './menu';
 
 // Booking/Meeting URL
-export const BOOKING_URL = 'tel:+91 72766 80893 ?utm_source=paidsearch&utm_campaign=GS_Attendance_India&utm_medium=google&utm_content=&utm_term=attendance%20marking%20system&hsa_acc=2719990364&hsa_cam=21025353964&hsa_grp=184919522347&hsa_ad=761638300479&hsa_src=g&hsa_tgt=kwd-307083052541&hsa_kw=attendance%20marking%20system&hsa_mt=p&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=21025353964&gbraid=0AAAAADM0n9SPExZp5fLVBsU0CKULK31k7&gclid=Cj0KCQiAjJTKBhCjARIsAIMC44-XRk91OpIus3JKQvih-8h0PSPdV6TO8PpUJBM_9DUsTFpTfcWj6wAaAqyAEALw_wcB';
+export const BOOKING_URL = 'tel:+91 72766 80893';
